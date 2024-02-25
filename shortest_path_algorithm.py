d9f6f8207bce12982c85399e423a9d1840fcdf07
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-02-25 12:59:54 +0100

    Started work on a 'shortest path algorithm'

---
+++ shortest_path_algorithm.py
@@ -0,0 +1,22 @@
+# Learning algorithm design by building s shortest path algorithm:
+# You'll learn how to use functions, loops, conditional statements, and dictionary comprehensions to implement a
+# Shortest Path algorithm.
+
+
+my_graph = {
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
+}
+
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
+
+# shortest_path(my_graph, 'A')
