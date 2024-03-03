fe0230de01ccf4ea39a7012bd6137625ef47f7ed
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-03-03 12:02:23 +0100

    Specified first two situations that will return error messages in 'project01_arithmetic_formatter.py'

---
+++ project01_arithmetic_formatter.py
@@ -0,0 +1,12 @@
+def arithmetic_arranger(problems, show_answers=False):
+
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    for problem in problems:
+        if "/" in problem or "*" in problem:
+            return "Error: Operator must be '+' or '-'."
+
+    return problems
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
