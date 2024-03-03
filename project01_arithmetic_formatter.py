9345f45aa3e34178ae45791314fc214f66cf7786
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-03-03 16:51:06 +0100

    Cleaned up the code and implemented the use of the 'show_answers=False' default argument

--- project01_arithmetic_formatter.py
+++ project01_arithmetic_formatter.py
@@ -1,13 +1,23 @@
 def arithmetic_arranger(problems, show_answers=False):
 
     if len(problems) > 5:
-        return "Error: Too many problems."
+        error_message = "Error: Too many problems."
+        if show_answers:
+            print(error_message)
+        return error_message
 
     for problem in problems:
         if "/" in problem or "*" in problem:
-            return "Error: Operator must be '+' or '-'."
+            error_message = "Error: Operator must be '+' or '-'."
+            if show_answers:
+                print(error_message)
+            return error_message
+
         elif " " not in problem[:5] or " " not in problem[-5:]:
-            return "Error: Numbers cannot be more than four digits."
+            error_message = "Error: Numbers cannot be more than four digits."
+            if show_answers:
+                print(error_message)
+            return error_message
 
     return problems
 
