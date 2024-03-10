b17c929ff8371a339d5c4f74d8d2d86212b63c82
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-03-10 11:39:11 +0100

    Implemented 'transfer()' method and added docstrings to class 'Category'

--- Cert_Projects/budget_app.py
+++ Cert_Projects/budget_app.py
@@ -1,11 +1,15 @@
 class Category:
+    """Represent a category for specific budget."""
 
     def __init__(self, name):
+        """Initialize name, ledger and balance attributes."""
         self.name = name
         self.ledger = []
         self.balance = 0
 
     def __str__(self):
+        """Return a formatted representation of the budget category, including ledger entries and the current
+        balance."""
         budget = self.name + "\n"
         for position in self.ledger:
             budget += position["description"] + " " + str(position["amount"]) + "\n"
@@ -13,30 +17,50 @@ class Category:
         return budget
 
     def deposit(self, amount, description=""):
+        """Take numeric value 'amount' and optional string 'description', insert them into 'self.ledger' and add
+        'amount' to 'self.budget'."""
         self.ledger.append({"amount": amount,
                             "description": description,
                             })
         self.balance += amount
 
     def withdraw(self, amount, description=""):
+        """Take numeric value 'amount' and optional string 'description', insert them into 'self.ledger' and subtract
+        'amount' from 'self.budget' if sufficient funds are available. Return 'True' if withdrawal took place, 'False'
+        if not."""
         check = self.check_funds(amount)
         if check:
-            amount = amount * (-1)
-            self.ledger.append({"amount": amount,
+            self.ledger.append({"amount": amount * (-1),
                                 "description": description,
                                 })
-            self.balance += amount
+            self.balance -= amount
             return True
         else:
             return False
 
     def get_balance(self):
+        """Return numeric value for 'self.balance'."""
         return self.balance
 
     def transfer(self, amount, other_category):
-        pass
+        """Transfer numeric value 'amount' to instance 'other_category', update 'self.ledger' and 'self.budget' in each
+        instance if sufficient funds are available and return 'True' if transfer took place, 'False' if not."""
+        check = self.check_funds(amount)
+        if check:
+            self.ledger.append({"amount": amount * (-1),
+                                "description": "Transfer to " + other_category.name,
+                                })
+            other_category.ledger.append({"amount": amount,
+                                          "description": "Transfer from " + self.name,
+                                          })
+            self.balance -= amount
+            other_category.balance += amount
+            return True
+        else:
+            return False
 
     def check_funds(self, amount):
+        """Check if numeric value 'amount' exceeds 'self.balance'."""
         if amount > self.balance:
             return False
         else:
@@ -48,9 +72,14 @@ def create_spend_chart(categories):
 
 
 # Instance and method calls for testing
-test = Category("TEST")
-test.deposit(50, "testdeposit")
-test.deposit(25)
-test.withdraw(10, "testwithdraw")
-test.withdraw(5)
-print(test)
+food = Category("FOOD")
+clothing = Category("CLOTHING")
+
+food.deposit(50, "food deposit")
+food.deposit(25)
+food.withdraw(10, "food withdraw")
+food.withdraw(5)
+food.transfer(10, clothing)
+
+print(food)
+print(clothing)
